b541f832b8045dfb8282334e38106fde169de555
prompt-base <158847398+prompt-base@users.noreply.github.com>
2025-11-06 20:42:48 +0530

    Update helpers.py

--- AntiDote/ANTIDOTE/shared/helpers.py
+++ AntiDote/ANTIDOTE/shared/helpers.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+def load_css(file_path: str):
+    """Load custom CSS file into Streamlit."""
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
