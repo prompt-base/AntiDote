63836e931f418c8da77f88b0e63843e1c3ab5be4
prompt-base <158847398+prompt-base@users.noreply.github.com>
2025-11-14 17:23:11 +0530

    Update Alzy--Beta.py

--- AntiDote/ANTIDOTE/pages/Alzy--Beta.py
+++ AntiDote/ANTIDOTE/pages/Alzy--Beta.py
@@ -620,7 +620,7 @@ st.markdown(
 
     /* Active tab label accent */
     div.stTabs [data-baseweb="tab"][aria-selected="true"] {
-      color: #ff4b4b !important;
+      color: #22d3ee !important;
     #   border-bottom: 3px solid #22d3ee !important;
     }
     </style>
